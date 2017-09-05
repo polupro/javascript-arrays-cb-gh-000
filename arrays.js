b9e2d0159e801158ed219dd06e836f63d1e3b607
Sergiy  Tkachuk <awertg@mail.ua>
2017-09-05 16:12:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-  array[3]=3;
+  var evenNumbers = new Array(3);
   element = "vadim tupit";
 function addElementToBeginningOfArray(array, element) {
-  
+
   array.unshift(`${element}`);
   console.log(array);
 }
+addElementToBeginningOfArray(evenNumbers, "dfsffh");
