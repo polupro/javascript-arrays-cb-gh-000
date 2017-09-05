d8780569a313037e70c144195a22f8e2bf32f69d
Sergiy  Tkachuk <awertg@mail.ua>
2017-09-05 16:10:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-  array[];
+  array[3]=3;
   element = "vadim tupit";
 function addElementToBeginningOfArray(array, element) {
+  
   array.unshift(`${element}`);
   console.log(array);
 }
